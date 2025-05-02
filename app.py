479cdf1aaf88c51505cf4ba3186e33368b1dd7d5
mada3959 <madain3959@gmail.com>
2025-05-02 16:49:37 +0700

    plants prediction in flask without sensor input

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-from flask import Flask, render_template, Response, request, jsonify, request
+from flask import Flask, render_template, Response, request, jsonify
 import pickle
 import numpy as np
 
 app = Flask(__name__)
-
-sensor_data = {
-    "cahaya": 0,
-    "suhu": 0,
-    "kelembapan_udara": 0,
-    "hujan": 0,
-    "kelembapan_tanah": 0
-}
-
 model = pickle.load(open('models/RandomForest.pkl', 'rb'))
 
 @app.route('/')
@@ -19,25 +10,28 @@ def index():
     return render_template('index.html')
 
 class_names = [
-    'rice', 'maize', 'chickpea', 'kidneybeans', 'pigeonpeas',
-    'mothbeans', 'mungbean', 'blackgram', 'lentil', 'pomegranate',
-    'banana', 'mango', 'grapes', 'watermelon', 'muskmelon', 'apple',
-    'orange', 'papaya', 'coconut', 'cotton', 'jute', 'coffee'
+    'Padi', 'Jagung', 'Kacang Arab', 'Kacang Merah', 'Kacang Gude',
+    'Kacang Moth', 'Kacang Hijau', 'Kacang Hitam', 'Kacang Lentil', 'Delima',
+    'Pisang', 'Mangga', 'Anggur', 'Semangka', 'Melon', 'Apel',
+    'Jeruk', 'Pepaya', 'Kelapa', 'Kapas', 'Goni', 'Kopi'
 ]
 
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
+        data = request.get_json()
         features = [
-            round(sensor_data['suhu'], 1),
-            round(sensor_data['kelembapan_udara'], 1),
-            round(sensor_data['hujan'], 1),
-            round(sensor_data['kelembapan_tanah'], 1)
+            round(float(data['suhu']), 1),
+            round(float(data['kelembapan_udara']), 1),
+            round(float(data['hujan']), 1),
+            round(float(data['kelembapan_tanah']), 1)
         ]
         input_array = np.array([features])
         prediction = model.predict(input_array)
         predicted_class = class_names[np.argmax(prediction)]
-
         return jsonify({'class': predicted_class})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
